691500bd2f0508ba2b6f0d077f4aed629310c9f9
Richard Li <lifuzu@gmail.com>
2018-08-25 00:44:53 -0700

    Start to ping/pong through socket.io

--- clients/extensions/chrome/js/socket.js
+++ clients/extensions/chrome/js/socket.js
@@ -9,3 +9,11 @@ socket.on('connect', function() {
 socket.on('disconnect', function(){
     console.log('client disconnected');
 });
+
+setInterval(function() {
+    socket.emit('event', { "users": "AAA", "time": new Date().getTime() });
+}, 3000);
+
+socket.on('notify', function(payload) {
+    console.log('payload: ' + JSON.stringify(payload));
+})
