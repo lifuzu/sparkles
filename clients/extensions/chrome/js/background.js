2dc3cdc5a1963631a7aa468f3eec6f4055204571
Richard Li <lifuzu@gmail.com>
2018-08-26 23:24:51 -0700

    Change host equals to schemes match

--- clients/extensions/chrome/js/background.js
+++ clients/extensions/chrome/js/background.js
@@ -46,7 +46,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
+                pageUrl: {schemes: ['https', 'http']},
             })],
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
